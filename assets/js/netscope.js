7f535c0a1c8e3bcaa54a977fb8fc3832f093ee94
Hui Liao <Brilliaoce@live.com>
2018-09-14 17:11:45 +0800

    change the depthwise macc

--- assets/js/netscope.js
+++ assets/js/netscope.js
@@ -16302,7 +16302,7 @@ module.exports = Analyzer = class Analyzer {
             //memory
             d.mem.param = (kernel_w * kernel_h) * d.chIn * d.chOut / group + has_bias * d.chOut; // convolutiondepthwise
           } else {
-            d.comp.macc = (kernel_w * kernel_h) * (d.wOut * d.hOut) * d.chIn * d.batchOut + (d.wOut * d.hOut) * d.chIn * d.chOut * d.batchOut / group;
+            d.comp.macc = (kernel_w * kernel_h) * (d.wOut * d.hOut) * d.chIn * d.batchOut; // + (d.wOut * d.hOut) * d.chIn * d.chOut * d.batchOut / group;
             d.mem.param = (kernel_w * kernel_h) * d.chIn + has_bias * d.chIn + d.chIn * d.chOut / group + has_bias * d.chOut;
           }
           d.mem.activation = (d.wOut * d.hOut) * d.chOut * d.batchOut;
